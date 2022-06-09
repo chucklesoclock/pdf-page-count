504b45525fafb589bca4eba210dd9e3f2a74be46
Charlie G <charlieg895@gmail.com>
2022-06-09 17:34:48 -0500

    make click cli, support -r and printing rich table

--- pdf_page_counter.py
+++ pdf_page_counter.py
@@ -4,6 +4,11 @@ from PyPDF2 import PdfReader
 from PyPDF2.errors import PdfReadError
 import logging
 
+import click
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+
 logging.basicConfig(
     filename="pdf_page_counter.log",
     filemode="w",
@@ -12,7 +17,7 @@ logging.basicConfig(
 )
 
 
-def count_page(path: pathlib.Path) -> int:
+def count_pages(path: pathlib.Path) -> int:
     try:
         with open(path.resolve(), mode="rb") as f:
             reader = PdfReader(f)
@@ -31,13 +36,61 @@ def count_page(path: pathlib.Path) -> int:
         sys.exit()
 
 
-def main():
-    total_pages = sum(
-        count_page(p) for p in pathlib.Path().iterdir() if p.suffix.lower() == ".pdf"
-    )
+@click.command()
+@click.argument(
+    "dir_path",
+    type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    default=pathlib.Path(),
+)
+@click.option(
+    "-r",
+    "--recursive",
+    is_flag=True,
+    default=False,
+    help="recursive search; count all pdfs in all subdirectors of DIR_PATH",
+)
+@click.option(
+    "-t",
+    "--table",
+    is_flag=True,
+    default=False,
+    help="print a table to terminal using rich",
+)
+def count(dir_path, recursive, table):
+    """
+    "1, 2, 3, PDFs do I see, HA HA HA"
+
+        -- Count von Count, Seasame Street
+    """
+    pdf_path_gen = getattr(dir_path, "rglob" if recursive else "glob")("*.pdf")
+
+    if not table:
+        total_pages = sum(count_pages(p) for p in pdf_path_gen)
+        print(total_pages)
+    else:
+        print()
+        table = Table(title="Count of PDF Pages", show_edge=True)
+        table.add_column(
+            Text.from_markup("PDF Path", justify="center"),
+            Text.from_markup("[u]Total Pages", justify="right", style="red"),
+            style="cyan",
+            justify="left",
+        )
+        table.add_column("Pages", str(0), justify="right", style="magenta")
+        total_pages = 0
+        for pdf_path in pdf_path_gen:
+            pages = count_pages(pdf_path)
+            table.add_row(str(pdf_path), str(pages))
+            total_pages += pages
+        table.show_footer = True
+        table.footer_style = "red"
+        table.columns[1].footer = f"[u]{total_pages}"
+        console = Console()
+        console.print(table)
+        print()
+
     logging.info(f"TOTAL_PAGE_COUNT={total_pages}")
-    print(total_pages)
 
 
 if __name__ == "__main__":
-    main()
+    count()
